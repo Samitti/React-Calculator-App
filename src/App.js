10438f317a6c8fd0221bae6ff6e3fdea1215a834
Samitti <samuelteweldebrhan@gmail.com>
2020-12-07 15:58:57 +0300

    remove un wanted files from src

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h3> Milestone One Completed.</h3>
       </header>
     </div>
